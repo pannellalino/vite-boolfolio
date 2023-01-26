cd28efd7d17f1094814f1e674413e388b47a59ba
pannellalino <lista>
2023-01-26 16:46:11 +0100

    Milestone 3-4

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script>
+
+import axios from 'axios';
+
+export default {
+  name: 'App',
+  data(){
+    return {
+      baseUrl: 'http://127.0.0.1:8000/api/'
+    }
+  },
+  methods:{
+    getApi(){
+      axios.get(this.baseUrl + 'projects')
+        .then(result =>{
+          console.log(result.data);
+        })
+    }
+  },
+  mounted(){
+    this.getApi();
+  }
+}
+</script>
+
+
+<template>
+  <div class="container">
+    <h1 class="text-success">Vue</h1>
+  </div>
+</template>
+
+
+<style lang="scss">
+@use './style/general.scss'
+</style>
