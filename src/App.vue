b4c3d9599563dd48573a08dfa645864bcfa8ae80
pannellalino <lista>
2023-01-26 17:55:33 +0100

    Milestone 5

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,24 @@
 <script>
 
 import axios from 'axios';
+import ProjectCard from "./components/ProjectCard.vue";
 
 export default {
   name: 'App',
+  components:{
+    ProjectCard
+  },
   data(){
     return {
-      baseUrl: 'http://127.0.0.1:8000/api/'
+      baseUrl: 'http://127.0.0.1:8000/api/',
+      projects: []
     }
   },
   methods:{
     getApi(){
       axios.get(this.baseUrl + 'projects')
         .then(result =>{
-          console.log(result.data);
+          this.projects = result.data.projects
         })
     }
   },
@@ -25,12 +30,15 @@ export default {
 
 
 <template>
-  <div class="container">
-    <h1 class="text-success">Vue</h1>
+  <div class="container text-center">
+    <h1 class="text-success p-5">Elenco progetti</h1>
+    <div class="d-flex flex-wrap justify-content-around">
+      <ProjectCard v-for="project in projects" :key="project.id" :project="project" />
+    </div>
   </div>
 </template>
 
 
 <style lang="scss">
-@use './style/general.scss'
+@use './style/general.scss';
 </style>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,32 @@
+<script>
+
+export default {
+  name: 'ProjectCard',
+  props: {
+    project: Object
+  },
+
+  data(){
+    return
+
+  }
+}
+</script>
+
+
+<template>
+    
+      <div class="card mb-4 bg-dark" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title text-muted">{{project.name}}</h5>
+          <h6 class="card-subtitle mb-2 text-muted">{{project.summary}}</h6>
+          <a href="#" class="">Show</a>
+        </div>
+      </div>
+
+</template>
+
+
+<style lang="scss">
+
+</style>
