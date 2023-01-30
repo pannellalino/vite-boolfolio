8bb117dc727935bc9a9e881d2fce5bd9d8a1b9e4
pannellalino <lista>
2023-01-30 16:41:04 +0100

    ul p-0

--- src/partials/Menu.vue
+++ src/partials/Menu.vue
@@ -7,7 +7,7 @@ export default {
 <template>
   <header>
     <nav>
-      <ul class="d-flex justify-content-center">
+      <ul class="d-flex justify-content-center p-0">
         <li><router-link :to="{name: 'home'}">Home</router-link></li>
         <li><router-link :to="{name: 'ProjectCard'}">Projects</router-link></li>
         <li><router-link :to="{name: 'about'}">About</router-link></li>
